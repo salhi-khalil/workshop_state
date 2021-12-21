1e716a7d05f322fcca35efd5be75d2df0d505ad2
khalil <salhikhalil2022@gmail.com>
2021-12-21 17:27:37 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProfileParent from './components/ProfileParent';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>React Class component</h1>
+      <ProfileParent />
+    </div>
+  );
+}
+
+export default App;
